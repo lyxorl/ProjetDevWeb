d874d93c8664812afb3cfd70df5fba741fbe2ecd
Alouhette <133207604+Alouhette@users.noreply.github.com>
2025-03-25 08:49:54 +0100

    Fichier SQL 0.1
    Fichier utilisable que pour créer les users (donner à ajouter quand mm)

---
+++ main/table.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Resident(
+	id INT PRIMARY KEY,
+	Nom VARCHAR(20),
+	Prenom VARCHAR(20));
+
+
+CREATE TABLE User(
+	id INT PRIMARY KEY,
+	Pseudo VARCHAR(20),
+    Age INT,
+    Genre VARCHAR(20),
+    Photo VARCHAR(20),
+    Rang VARCHAR(20),
+    Nom VARCHAR(20),
+    Prenom VARCHAR(20),
+    MDP VARCHAR(64),
+    CONSTRAINT  PRIMARY KEY (Prenom,Nom),
+	FOREIGN KEY fk_Resident(Prenom) REFERENCES Resident(Prenom), 
+	FOREIGN KEY fk_Resident(Nom) REFERENCES Resident(Nom));
